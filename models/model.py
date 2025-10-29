1d8a05a8effb0bf6ee4e4dd4dc75efef40639818
Kapil Patil <patilkapil96kp5@gmail.com>
2025-10-29 14:50:03 +0530

    models with two tables event and attendee without relation

---
+++ models/model.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import enum 
+
+db = SQLAlchemy()
+
+class eventstatus(enum.Enum):
+    Online ='online'
+    offline='offline'
+
+class event(db.Model):
+    __tablename__ = 'events'
+    id = db.column(db.Integer,primary_key=True)
+    title=db.column(db.String(100),nullable=False)
+    date=db.column(db.Date,nullable=False)
+    time=db.column(db.Time)
+    mode=db.column(db.Enum(eventstatus),default = eventstatus.online,nullable=False)
+    venue=db.column(db.string(100),nullable=True)
+
+    def __repr__(self):
+        return f"<event {self.title} >"
+
+class Attendee(db.model):
+    __tablename__ ='attendee'
+
+    id = db.column(db.Integer,primary_key=True)
+    name = db.column(db.string(100),nullable=False)
+    email = db.column(db.string(120),unique=True,nullable=False)
+    phone = db.column(db.string(10))
+    address=db.column(db.string(200),nullable=False)
+
+    def __repr__(self):
+        return f"<Attendee {self.title} >" 
