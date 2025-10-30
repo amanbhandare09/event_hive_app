a297d274a3194a11f2d654d6823fd814d41ef42c
Gauravi Kamat <gauravikamat77@gmail.com>
2025-10-31 00:34:30 +0530

    instance of db

--- app/__init__.py
+++ app/__init__.py
@@ -1,34 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, text
 
 db = SQLAlchemy()
-
-def create_database_if_not_exists():
-    engine = create_engine("mysql+pymysql://root:@localhost")
-    db_name = "eventdb"
-
-    with engine.connect() as conn:
-        result = conn.execute(text(f"SHOW DATABASES LIKE '{db_name}'")).fetchone()
-        if not result:
-            conn.execute(text(f"CREATE DATABASE {db_name}"))
-            print(f"Database '{db_name}' created successfully.")
-        else:
-            print(f"Database '{db_name}' already exists.")
 
 def create_app():
     app = Flask(__name__)
 
     # Database configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/eventdb'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = '91ba96dbf66221f753176c6a80f0e7905444d410630b70f89445e7281d0001aa'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-    create_database_if_not_exists()
     db.init_app(app)
 
-    with app.app_context():
-        from models.model import Event, User
-        db.create_all()
-        print("Tables created successfully.")
+    from .main import main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    from .auth import auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     return app
